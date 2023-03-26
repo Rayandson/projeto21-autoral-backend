b935f0ef576f97f460a2cb303c63b53e5bc4a408
Rayandson <rayandson.silva321@gmail.com>
2023-03-26 00:20:53 -0300

    fix: test change importations

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,4 @@
-import express, { Express } from "express";
-import cors from "cors";
-import { loadEnv, connectDb, disconnectDB } from "@/config";
-// import { handleApplicationErrors } from "@/middlewares";
-import {
-  usersRouter
-} from "@/routers";
-import { restaurantsRouter } from "./routers/restaurantsRouter";
-
-loadEnv();
-
-const app = express();
-app
-  .use(cors())
-  .use(express.json())
-  .get("/health", (_req, res) => res.send("OK!"))
-  .use("/users", usersRouter)
-  .use("/restaurants", restaurantsRouter)
-//   .use(handleApplicationErrors);
-
-export function init(): Promise<Express> {
-  connectDb();
-  return Promise.resolve(app);
-}
-
-export async function close(): Promise<void> {
-  await disconnectDB();
-}
+import app, { init } from "./app";
 
 const port = +process.env.PORT || 4000;
 
@@ -35,5 +8,3 @@ init().then(() => {
     console.log(`Server is listening on port ${port}.`);
   });
 });
-
-export default app;
