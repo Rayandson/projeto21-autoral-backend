6a00a7aad19ca9b4bb31b286d5635968ce8dfcfd
Rayandson <rayandson.silva321@gmail.com>
2023-04-13 12:35:23 -0300

    fix: token authentication on history router

--- src/routers/authenticationRouter.ts
+++ src/routers/authenticationRouter.ts
@@ -2,10 +2,9 @@ import { validateBody } from "../middlewares/validationMiddleware";
 import { Router } from "express";
 import { signIn } from "../controllers";
 import { signInSchema } from "../schemas/authenticationSchema";
-import { authenticateToken } from "../middlewares/authenticationMiddleware";
 
 const authenticationRouter = Router();
 
-authenticationRouter.post("/signin", authenticateToken, validateBody(signInSchema), signIn);
+authenticationRouter.post("/signin", validateBody(signInSchema), signIn);
 
 export { authenticationRouter };
--- src/routers/ordersRouter.ts
+++ src/routers/ordersRouter.ts
@@ -2,12 +2,13 @@ import { validateBody } from "../middlewares/validationMiddleware";
 import { getOrder, getOrdersByUserId, postOrder } from "../controllers/ordersController";
 import { Router } from "express";
 import { ordersSchema } from "../schemas/ordersSchema";
+import { authenticateToken } from "../middlewares/authenticationMiddleware";
 
 const ordersRouter = Router();
 
 ordersRouter
   .post("/", validateBody(ordersSchema), postOrder)
   .get("/:id", getOrder)
-  .get("/history", getOrdersByUserId);
+  .get("/history", authenticateToken, getOrdersByUserId);
 
 export { ordersRouter };
