05fe75a77c0674d47f3307bcc35acb6853800f4a
Rayandson <rayandson.silva321@gmail.com>
2023-04-15 16:39:23 -0300

    fix: send correct error while logging in

--- src/controllers/authenticarionController.ts
+++ src/controllers/authenticarionController.ts
@@ -14,7 +14,7 @@ export async function signIn(req: Request, res: Response) {
       return res.sendStatus(httpStatus.UNAUTHORIZED);
     } else {
       console.log(err);
-      return res.status(404).send(err);
+      return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 }
